7cd7e7c952be89a355a956d7d5ab74feb80ce647
JoseCarlos122 <josecarloscallanchoamato4@gmail.com>
2023-09-12 22:15:30 -0400

    first commit

---
+++ src/main/java/com/emergentes/modelo/Producto.java
@@ -0,0 +1,52 @@
+package com.emergentes.modelo;
+public class Producto {
+    private int id;
+    private String descripcion;
+    private int cantidad;
+    private double precio;
+    private String Categoria;
+    public Producto() {
+        this.id = 0;
+        this.descripcion = "";
+        this.cantidad = 0;
+        this.precio = 0;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public double getCategoria() {
+        return precio;
+    }
+
+    public void setCategoria(String Categoria) {
+        this.Categoria = Categoria;
+    }    
+}
